01ba42dd00d3b51802988814c522f4d5b59775dd
bendtherules <abhasbhattacharya2@gmail.com>
2020-10-16 12:25:21 +0530

    MDNBadge - Add component

---
+++ components/MDNBadge.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./MDNBadge.module.css";
+
+export default function MDNBadge({ title, url, i }) {
+  // i stands for "is inline?" - default false
+  const wrapperClass = i ? wrapperInline : styles.wrapperBlock;
+  const imgClass = i ? imgInline : styles.imgBlock;
+
+  return (
+    <a
+      href={url}
+      className={wrapperClass}
+      target="_blank"
+      rel="noopener noreferer"
+    >
+      <img
+        className={imgClass}
+        src={`https://img.shields.io/badge/MDN-${title}-c2e8f9?logo=MDN%20Web%20Docs&style=flat-square`}
+      />
+    </a>
+  );
+}
+
+MDNBadge.defaultProps = {
+  i: false,
+};
