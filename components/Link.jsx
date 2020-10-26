4117131c12f2b68bbf954a58feb784b067ebc143
bendtherules <abhasbhattacharya2@gmail.com>
2020-10-26 17:23:40 +0530

    Link - reformat

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,15 +1,17 @@
-import React from "react"
+import React from "react";
 
-const Link = ({hash,title}) =>{
+const Link = ({ hash, title }) => {
+  const handleLink = (event) => {
+    //manually change hash to trigger hashChange event.
+    window.location.hash = "#";
+    window.location.hash = hash;
+  };
 
-    const handleLink=(event)=>{
-        //manually change hash to trigger hashChange event.
-        window.location.hash  = "#";
-        window.location.hash = hash;
-      }
-      
-      
-    return <a onClick={handleLink} className="navlink">{title}</a>
-}
+  return (
+    <a onClick={handleLink} className="navlink">
+      {title}
+    </a>
+  );
+};
 
-export default Link
+export default Link;
